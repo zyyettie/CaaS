7ee753cbafce91537d2f65f80fcc425a4cf711c0
Johnson Jiang <lizwjiang@gmail.com>
2015-07-23 19:46:42 +0800

    add rules for web.xml add list method in WebApp

--- caas-core/src/main/java/org/g6/caas/xml/web/WebApp.java
+++ caas-core/src/main/java/org/g6/caas/xml/web/WebApp.java
@@ -2,6 +2,7 @@ package org.g6.caas.xml.web;
 
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -23,4 +24,55 @@ public class WebApp {
     private List<Servlet> servlets;
     private List<ServletMapping> servletMappings;
     private List<ErrorPage> errorPages;
+
+    public void addContextParam(ContextParam param){
+        if(contextParams == null)
+            contextParams = new ArrayList<>();
+
+        contextParams.add(param);
+    }
+
+    public void addMimeMapping(MimeMapping mimeMapping){
+        if(mimeMappings == null)
+            mimeMappings = new ArrayList<>();
+
+        mimeMappings.add(mimeMapping);
+    }
+    public void addFilter(Filter filter){
+        if(filters == null)
+            filters = new ArrayList<>();
+
+        filters.add(filter);
+    }
+
+    public void addFilterMapping(FilterMapping filterMapping){
+        if(filterMappings == null)
+            filterMappings = new ArrayList<>();
+
+        filterMappings.add(filterMapping);
+    }
+    public void addListener(Listener listener){
+        if(listeners == null)
+            listeners = new ArrayList<>();
+
+        listeners.add(listener);
+    }
+    public void addServlet(Servlet servlet){
+        if(servlets == null)
+            servlets = new ArrayList<>();
+
+        servlets.add(servlet);
+    }
+    public void addServletMapping(ServletMapping servletMapping){
+        if(servletMappings == null)
+            servletMappings = new ArrayList<>();
+
+        servletMappings.add(servletMapping);
+    }
+    public void addServlet(ErrorPage errorPage){
+        if(errorPages == null)
+            errorPages = new ArrayList<>();
+
+        errorPages.add(errorPage);
+    }
 }
