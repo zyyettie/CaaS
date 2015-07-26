85a88ccdbba75be6fb9e92cb9fabd7ba4fd65606
Johnson Jiang <lizwjiang@gmail.com>
2015-07-26 13:46:22 +0800

    add more rules in web-rule.xml for web.xml

---
+++ caas-core/src/main/java/org/g6/caas/xml/web/T.java
@@ -0,0 +1,11 @@
+package org.g6.caas.xml.web;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jili
+ * Date: 7/26/15
+ * Time: 1:46 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class T {
+}
